1592d6653f833edef77c8db6ce6d3067694a530e
Luke <94718478+Noelialuke@users.noreply.github.com>
2022-05-03 11:51:50 +0200

    Update app.py

--- proyecto/app.py
+++ proyecto/app.py
@@ -7,7 +7,29 @@ import joblib
 app = Flask(__name__)
 
 # Main function here
-# ------------------
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    
+    # If a form is submitted
+    if request.method == "POST":
+        
+        # Unpickle classifier
+        clf = joblib.load("clf.pkl")
+        
+        # Get values through input bars
+        height = request.form.get("Altura")
+        weight = request.form.get("Peso")
+        
+        # Put inputs to dataframe
+        X = pd.DataFrame([[Altura, Peso]], columns = ["Altura", "Peso"])
+        
+        # Get prediction
+        prediction = clf.predict(X)[0]
+        
+    else:
+        prediction = ""
+        
+    return render_template("website.html", output = prediction)
 
 # Running the app
 if __name__ == '__main__':
